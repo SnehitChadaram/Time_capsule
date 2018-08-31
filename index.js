a12f5c2065f52adf64b2e894dc86ff2f55661ae7
Chadaram <snehit.chadaram@hpe.com>
2018-08-31 14:15:20 +0530

    With simple email login enable

---
+++ index.js
@@ -0,0 +1,79 @@
+var mainText = document.getElementById("mainText");
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick()
+{
+  var firebaseRef = firebase.database().ref();
+  var messageText = mainText.value;
+  firebaseRef.push().set(messageText);
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    $(".login-cover").hide();
+
+    var dialog = document.querySelector('#loginDialog');
+    /*
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    */
+    dialog.close();
+
+  } else {
+
+    $(".login-cover").show();
+
+    // No user is signed in.
+    var dialog = document.querySelector('#loginDialog');
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.showModal();
+
+  }
+});
+
+
+/* LOGIN PROCESS */
+
+$("#loginBtn").click(
+  function(){
+
+
+    var email = $("#loginEmail").val();
+    var password = $("#loginPassword").val();
+
+    if(email != "" && password != ""){
+      $("#loginProgress").show();
+      $("#loginBtn").hide();
+
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        $("#loginError").show().text(errorMessage);
+        $("#loginProgress").hide();
+        $("#loginBtn").show();
+      });
+    }
+  }
+);
+
+
+/* LOGOUT PROCESS */
+
+$("#signOutBtn").click(
+  function(){
+
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+      alert(error.message);
+    });
+
+  }
+);
